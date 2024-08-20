1fc74c4be54c07603a2f9a5996eefb9adfbc396b
venixlb <105927381+venixlb@users.noreply.github.com>
2024-08-21 00:46:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client } = require('discord.js-selfbot-v13');
 const client = new Client(); 
 const express = require("express");
 const app = express();
-const { joinVoiceChannel } = require('@discordjs/voice');
+const { joinVoiceChannel, getVoiceConnection } = require('@discordjs/voice');
 
 // إنشاء السيرفر باستخدام Express
 var listener = app.listen(process.env.PORT || 2000, function () {
@@ -23,17 +23,38 @@ client.on('ready', async () => {
   // جلب القناة الصوتية من السيرفر
   const channel = await client.channels.fetch(process.env.channel);
   
-  // التحقق من إذا كنت موجودًا بالفعل في القناة الصوتية
-  if (!channel.members.has(client.user.id)) {
-    const VoiceConnection = joinVoiceChannel({
-      channelId: channel.id, 
-      guildId: process.env.guild, 
-      selfMute: true,
-      selfDeaf: true,
-      adapterCreator: channel.guild.voiceAdapterCreator 
-    });
+  // الاتصال بالقناة الصوتية إذا لم يكن البوت موجودًا فيها
+  connectToChannel(channel);
+});
+
+// مستمع لأحداث الصوت لمعرفة متى يخرج البوت من القناة
+client.on('voiceStateUpdate', (oldState, newState) => {
+  // التحقق من أن المستخدم هو البوت نفسه
+  if (oldState.member.id === client.user.id) {
+    const channel = oldState.guild.channels.cache.get(process.env.channel);
+    
+    // إذا خرج البوت من القناة الصوتية
+    if (!newState.channelId) {
+      console.log("Bot left the voice channel. Reconnecting...");
+      connectToChannel(channel);
+    }
   }
 });
+
+// دالة للاتصال بالقناة الصوتية
+function connectToChannel(channel) {
+  const connection = getVoiceConnection(channel.guild.id);
+  
+  if (!connection) {
+    joinVoiceChannel({
+      channelId: channel.id,
+      guildId: channel.guild.id,
+      selfMute: true,
+      selfDeaf: true,
+      adapterCreator: channel.guild.voiceAdapterCreator,
+    });
+  }
+}
 
 // تسجيل الدخول إلى حساب Discord
 client.login(process.env.token);
