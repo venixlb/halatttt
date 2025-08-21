b9075abc5849eb4bca2d6f927560dd36581f86be
venixlb <105927381+venixlb@users.noreply.github.com>
2025-08-22 02:44:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ function createBot() {
     host: 'Tinatix.aternos.me',
     port: 30806,
     username: '24h',
-    version: 'auto' // 👈 يخلي mineflayer يختار النسخة المناسبة تلقائي
+    version: '1.21.8' // 👈 النسخة الجديدة
   });
 
   bot.on('spawn', () => {
     console.log('✅ Bot is online!');
-    // Anti-AFK
     setInterval(() => {
       bot.setControlState('jump', true);
       setTimeout(() => bot.setControlState('jump', false), 250);
@@ -19,7 +18,6 @@ function createBot() {
 
   bot.on('kicked', (reason) => console.log('⛔ Kicked:', reason));
   bot.on('error', (err) => console.log('⚠️ Error:', err));
-
   bot.on('end', () => {
     console.log('❌ Bot disconnected, reconnecting in 10s...');
     setTimeout(createBot, 10000);
