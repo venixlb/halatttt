71b7bad046eb33b80c17cebf3dff4d41cb1debc0
venixlb <105927381+venixlb@users.noreply.github.com>
2025-08-22 02:34:07 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => res.send('Bot is running!'));
+
+app.listen(PORT, () => console.log(`🌐 Web server running on port ${PORT}`));
